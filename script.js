980a8b4f7cc9759712b2c3f49885f08893054547
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-12-07 13:33:41 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your code here
+let dragged = null;
+const boxes = document.querySelectorAll(".image");
+
+boxes.forEach((box) => {
+  box.addEventListener("dragstart", () => {
+    dragged = box;
+    box.classList.add("selected");
+  });
+
+  box.addEventListener("dragend", () => {
+    box.classList.remove("selected");
+  });
+
+  box.addEventListener("dragover", (event) => {
+    event.preventDefault(); // allow drop
+  });
+
+  box.addEventListener("drop", (e) => {
+    e.preventDefault();
+    if (dragged !== box) {
+      // get current background images from computed style
+      const draggedBg = getComputedStyle(dragged).backgroundImage;
+      const boxBg = getComputedStyle(box).backgroundImage;
+
+      // set them as inline styles (swap)
+      dragged.style.backgroundImage = boxBg;
+      box.style.backgroundImage = draggedBg;
+    }
+  });
+});
